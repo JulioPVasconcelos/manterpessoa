7e973d58e81a45b5770877d7ea8f03d838648998
Julio Vasconcelos <julio_jpv@yahoo.com.br>
2025-03-25 20:42:10 -0300

    Adiciona função remover tarefa

---
+++ tarefas.py
@@ -0,0 +1,8 @@
+lista_tarefas = []
+
+def adicionar_tarefa(tarefa: str):
+    lista_tarefas.append(tarefa)
+
+def remover_tarefa(tarefa: str):
+    if tarefa in lista_tarefas:
+        lista_tarefas.remove(tarefa)
